76b8191d94e197da950295e5a9b1880ba844c893
mimaay <yemimaokv31@gmail.com>
2026-01-12 09:43:06 +0700

    Add files via upload

---
+++ X RPL/KELAS/Kelas Pak Tri/12-01-2026/XRPL.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package x.rpl;
+
+/**
+ *
+ * @author MyPC PRO
+ */
+public class XRPL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int angka;
+        angka=10;
+        
+        System.out.println(angka);
+        System.out.println("angka yang anda inputkan:"+angka);
+        angka=100;
+        System.out.println("angka terbaru adalah:"+angka);
+        angka=1000;
+        System.out.println("angka yang anda inputkan:"+angka);
+        angka=10000;
+        System.out.println("angka terbaru adalah:"+angka);
+        angka=100000;
+        System.out.println("angka yang anda inputkan:"+angka);
+        angka=1000000;
+        System.out.println("angka terbaru adalah:"+angka);
+        
+        double desimal=0.31;
+        System.out.println("angka yang anda inputkan:"+desimal);
+        desimal=0.50;
+        System.out.println("angka terbaru adalah:"+desimal);
+        float pecahan=31.1009f;
+        System.out.println("angka yang anda inputkan:"+pecahan);
+        pecahan=09.1031f;
+        System.out.println("angka terbaru adalah:"+pecahan);
+        
+        String nama= "Stela";
+        char abjad='A';
+        System.out.println("Nama:"+nama);
+        System.out.println("Abjad:"+abjad);
+        
+        boolean  benar=true;
+        boolean salah= false;
+        System.out.println("benar");
+        System.out.println("salah");
+        
+        //desimal=53.35453f;
+        System.out.println("desimal");
+        
+        
+    }
+    
+}
